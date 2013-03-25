083b68e51a338014c508c4140c2d0fd784e26ad1
Tobias Klassen <tobiasklassen@gmail.com>
2013-03-25 20:17:40 +0100

    first commit

--- application/routes.php
+++ application/routes.php
@@ -37,6 +37,13 @@ Route::get('/', function()
 	return View::make('home.index');
 });
 
+Route::get('/login', function()
+{
+	echo("login");
+});
+
+
+
 /*
 |--------------------------------------------------------------------------
 | Application 404 & 500 Error Handlers
